4a824089a91f26ff5c7939ca7f4943143eb16a1a
Leonardo Carvalho <0leonardocarvalho@gmail.com>
2025-03-22 21:27:13 -0300

    primeiro commit

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,52 @@
+import Button from '../Button'
+import {
+  Overlay,
+  CartContainer,
+  Sidebar,
+  Quantity,
+  Prices,
+  CartItem
+} from './styles'
+import zelda from '../../assets/images/zelda.png'
+import Tag from '../Tag'
+
+const Cart = () => {
+  return (
+    <CartContainer>
+      <Overlay />
+      <Sidebar>
+        <ul>
+          <CartItem>
+            <img src={zelda} />
+            <div>
+              <h3>Nome do jogo</h3>
+              <Tag>RPG</Tag>
+              <Tag>PS5</Tag>
+              <span>R$ 150,00</span>
+            </div>
+            <button type="button" />
+          </CartItem>
+          <CartItem>
+            <img src={zelda} />
+            <div>
+              <h3>Nome do jogo</h3>
+              <Tag>RPG</Tag>
+              <Tag>PS5</Tag>
+              <span>R$ 150,00</span>
+            </div>
+            <button type="button" />
+          </CartItem>
+        </ul>
+        <Quantity>2 jogos no carrinho</Quantity>
+        <Prices>
+          Total de R$ 250,00 <span>EM até 6x sem juros</span>
+        </Prices>
+        <Button title="Clique aqui para continuar com a compra" type="button">
+          Continuar com a compra
+        </Button>
+      </Sidebar>
+    </CartContainer>
+  )
+}
+
+export default Cart
---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import ProductsList from '../../components/ProductsList'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+import { Game } from '../Home'
+
+import {
+  useGetActionGamesQuery,
+  useGetFightGamesQuery,
+  useGetRpgGamesQuery,
+  useGetSimulationGamesQuery,
+  useGetSportGamesQuery
+} from '../../services/api'
+
+const Categories = () => {
+  const { data: actionGames } = useGetActionGamesQuery()
+  const { data: fightGames } = useGetFightGamesQuery()
+  const { data: rpgGames } = useGetRpgGamesQuery()
+  const { data: simulationGames } = useGetSimulationGamesQuery()
+  const { data: sportGames } = useGetSportGamesQuery()
+
+  if (actionGames && fightGames && rpgGames && simulationGames && sportGames) {
+    return (
+      <>
+        <ProductsList games={actionGames} title="Ação" background="black" />
+        <ProductsList games={sportGames} title="Esportes" background="gray" />
+        <ProductsList games={fightGames} title="Luta" background="black" />
+        <ProductsList games={rpgGames} title="RPG" background="black" />
+        <ProductsList
+          games={simulationGames}
+          title="Simulação"
+          background="gray"
+        />
+      </>
+    )
+  }
+
+  return <h4>Carregando...</h4>
+}
+
+export default Categories
